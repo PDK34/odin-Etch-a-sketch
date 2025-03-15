9b712b8f394e2e1c8cfd2acef4b76fe722eb18bd
Parthip <projectcs34@gmail.com>
2025-03-15 19:03:20 +0530

    Add a prompt button and modify grid container display
    Prompt allows user to change sizing of grid squares.Container display is now based on viewport height and scrolling is prevented.

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
-
 const container = document.querySelector('.container');
 
-let sqSize= 16;
-let rowNum = 960/sqSize;
-let colNum = 960/sqSize;
-for(let i =0;i<rowNum;i++){
-    for (let j = 0; j < colNum; j++) {
+let sqNum = 16;
+function createGrid(rowNum, colNum) {
+    container.innerHTML = '';
+    let sqSize = Math.floor(Math.min(container.clientWidth, container.clientHeight) )/ rowNum;
+    
+    for (let i = 0; i < rowNum; i++) {
+        for (let j = 0; j < colNum; j++) {
             let div = document.createElement('div');
             div.style.boxSizing = 'border-box';
-            div.style.display='flex';
-            div.style.width='16px';
-            div.style.height = '16px';
+            div.style.display = 'flex';
+            div.style.width = `${sqSize}px`;  
+            div.style.height = `${sqSize}px`;
             div.style.border = '1px solid black';
             container.appendChild(div);
-
+    
             div.addEventListener('mouseenter', () => {
-                div.classList.toggle('hovereffect')
+                div.classList.toggle('hovereffect');
             });
 
-            div.addEventListener('mouseleave' ,(e) => {
-               div.classList.toggle('hovereffect');
-            
-            })
-        
+            div.addEventListener('mouseleave', () => {
+                div.classList.toggle('hovereffect');
+            });
+        }
     }
 }
 
-
-container.addEventListener('click' ,(e) => {
+container.addEventListener('click', (e) => {
     let square = e.target;
-   if (square!==container) {
-     square.classList.toggle('color');
-   }
+    if (square !== container) {
+        square.classList.toggle('color');
+    }
+});
 
-})
-
-
-
-
-function clearGrid(){
+function clearGrid() {
     Array.from(container.children).forEach((child) => {
-    child.classList.remove('color','hovereffect')}
-    )
+        child.classList.remove('color', 'hovereffect');
+    });
 }
 
 const clearBtn = document.querySelector('.clearbtn');
+clearBtn.addEventListener('click', clearGrid);
 
-clearBtn.addEventListener('click',clearGrid)
+function changeGrid() {
+    let sqNum = parseInt(prompt("Enter number of squares in grid:", 16));
+    if (!isNaN(sqNum) && sqNum > 0 && sqNum <= 100) {
+        createGrid(sqNum, sqNum);
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
+}
+
+createGrid(sqNum,sqNum);
